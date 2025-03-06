544e61181291249b767c530dfb1d4d2664fd91c2
UrickL <108941452+UrickL@users.noreply.github.com>
2025-03-06 09:48:42 -0300

    Update VotingCategories.ts

--- src/Data/VotingCategories.ts
+++ src/Data/VotingCategories.ts
@@ -8,13 +8,14 @@ export function getVotingCategoriesData() {
     LÍDER: {
       digits: 2,
       candidates: [
-        {Id: 11, Nome: "Nivia", Partido: "Breadzil", pictureUrl: "nivia.jpeg"},
-        {Id: 22, Nome: "Beatriz Aranha", Partido: "Brapzza", pictureUrl: "bia.jpeg"},
-        {Id: 33, Nome: "Maria Luiza", Partido: "Massa", pictureUrl: "malu.jpeg"}
-		{Id: 44, Nome: "Breno Almeida", Partido: "Breadzil", pictureUrl: "breno.jpeg"},
-        {Id: 55, Nome: "Pedro Antônio", Partido: "Brapzza", pictureUrl: "pedro.jpeg"},
-        {Id: 66, Nome: "Juan Maracajá", Partido: "Massa", pictureUrl: "juan.jpeg"}
-		{Id: 77, Nome: "João Gabriel", Partido: "Massa", pictureUrl: "joao.jpeg"}
+       {Id: 11, Nome: "Nivia Gabrielle", Partido: "ETERNAL WATER", pictureUrl: "nivia.jpeg"},
+        {Id: 22, Nome: "Beatriz Aranha", Partido: "RADIANT FIRE", pictureUrl: "bia.jpeg"},
+        {Id: 33, Nome: "Maria Luiza", Partido: "ETERNAL WATER", pictureUrl: "malu.jpeg"}
+		{Id: 44, Nome: "Breno Almeida", Partido: "GLORIOUS LIGHT", pictureUrl: "breno.jpeg"},
+        {Id: 55, Nome: "Pedro Antônio", Partido: "GLORIOUS LIGHT", pictureUrl: "pedro.jpeg"},
+        {Id: 66, Nome: "Juan Maracajá", Partido: "NATURAL GREEN", pictureUrl: "juan.jpeg"}
+		{Id: 77, Nome: "João Gabriel", Partido: "NATURAL GREEN", pictureUrl: "joao.jpeg"}
+		{Id: 88, Nome: "Sofia Alexandre", Partido: "RADIANT FIRE", pictureUrl: "sofia.jpeg"}
       ]
 }
     
